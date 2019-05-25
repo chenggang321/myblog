9bc81ca2905105c89b4f9b3aedfca15d8501a79e
chenggang321 <1129137164@qq.com>
2019-05-25 12:41:41 +0800

    数据库连接修改

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use('/api',require('./routers/api'));
 app.use('/',require('./routers/main'));
 
 //连接数据库
-mongoose.connect('mongodb://47.94.208.33:27017/test',function(err){
+mongoose.connect('mongodb://localhost:27017/test',function(err){
     if(err){
         console.log("数据库连接失败");
     }else{
