703d588ece0988a6da1aaaa02cc47dd824211cbf
chenggang <chenggang@hua-yong.com>
2020-09-24 18:11:16 +0800

    授权添加

--- config/index.js
+++ config/index.js
@@ -1,5 +1,7 @@
 module.exports = {
   appID: 'wxf8669c8e8bd05427',
+  //appID: 'wxeed996884836a7b6',
   appSecret: '979f7818184d86a5eb0cbe6b722c1212',
+  //appSecret: 'ec150ebca74fa795b410c970f8095361',
   token: 'test'
 }
