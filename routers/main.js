f45310f65f1d3e0b2f0a87a087f762d6e1abe59c
chenggang <chenggang@hua-yong.com>
2020-09-24 14:46:25 +0800

    微信验证接口添加

--- routers/main.js
+++ routers/main.js
@@ -1,4 +1,5 @@
 var express = require('express')
+var crypto = require('crypto')
 var router = express.Router()
 var Category = require('../models/Category')
 var Content = require('../models/Content')
@@ -84,11 +85,29 @@ router.get('/view', function(req, res) {
   })
 })
 
-/*// 微信sdk
+// 微信sdk
 router.get('/weixin', function(req, res) {
-  res.json({
-    success: true
-  })
-})*/
+  //1.获取微信服务器Get请求的参数 signature、timestamp、nonce、echostr
+  var signature = req.query.signature, //微信加密签名
+    timestamp = req.query.timestamp, //时间戳
+    nonce = req.query.nonce, //随机数
+    echostr = req.query.echostr //随机字符串
+
+  //2.将token、timestamp、nonce三个参数进行字典序排序
+  var array = ['test', timestamp, nonce]
+  array.sort()
+
+  //3.将三个参数字符串拼接成一个字符串进行sha1加密
+  var tempStr = array.join('')
+  var hashCode = crypto.createHash('sha1') //创建加密类型
+  var resultCode = hashCode.update(tempStr, 'utf8').digest('hex') //对传入的字符串进行加密
+  console.log(signature)
+  //4.开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
+  if (resultCode === signature) {
+    res.send(echostr)
+  } else {
+    res.send('mismatch')
+  }
+})
 
 module.exports = router
